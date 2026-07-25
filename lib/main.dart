91d531a56dbf3aab4d06a45de9e5b2fd05e3d890
Bernard <judelsbernard2@gmail.com>
2026-07-25 19:02:15 +0200

    using containers and safeArea

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,21 @@ class MyWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.teal[100],
         appBar: AppBar(
           title: const Center(child: Text("Expence Tracker")),
           backgroundColor: Colors.teal[300],
         ),
-        body: const Center(
-          child: Image(image: AssetImage('images/automatic-payment.png')),
+        body: SafeArea(
+          child: Container(
+            color: Colors.pink[200],
+            height: 150.0,
+            width: 150.0,
+            padding: EdgeInsets.all(10.0),
+
+            child: Text("Monthly Debit Orders"),
+          ),
         ),
-        backgroundColor: Colors.teal[100],
       ),
     );
   }
