6f252378bc8471ad9d29313641ad4832e1c963c6
Bernard <judelsbernard2@gmail.com>
2026-07-26 00:28:24 +0200

    created a seperate file and class in order to help me with code reusability for the UI in the future

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:first_flutter_app/uiTools.dart';
 
 void main() {
   runApp(MyWidget());
 }
 
 class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+  MyWidget({super.key});
+
+  final uiTools = Uitools();
 
   @override
   Widget build(BuildContext context) {
@@ -18,122 +21,117 @@ class MyWidget extends StatelessWidget {
         ),
         body: SafeArea(
           child: Center(
-            child: Container(
-              // color: Colors.amber[100],
-              width: double.infinity,
-              // height: 100,
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Row(
+            child: Column(
+              children: [
+                Container(height: 40),
+
+                SizedBox(height: 10),
+
+                Container(
+                  // color: Colors.amber[100],
+                  width: double.infinity,
+
+                  child: Center(
+                    child: Column(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        Expanded(
-                          child: Center(
-                            child: Container(
-                              padding: const EdgeInsets.all(10.0),
-                              decoration: BoxDecoration(
-                                color: Colors.pink[100],
-                                borderRadius: BorderRadius.circular(20),
-                              ),
-                              child: Column(
-                                children: [
-                                  Text("Monthly Debit Orders"),
-
-                                  SizedBox(height: 20),
-
-                                  OutlinedButton(
-                                    onPressed: () {},
-                                    child: const Text("Cancel"),
-                                  ),
-                                ],
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Expanded(
+                              child: Center(
+                                child: uiTools.imgBtnTitleContainer(
+                                  "Monthly Debit Orders",
+                                  "images/automatic-payment.png",
+                                ),
                               ),
                             ),
-                          ),
+
+                            Expanded(
+                              child: Center(
+                                child: Container(
+                                  padding: const EdgeInsets.all(10.0),
+                                  decoration: BoxDecoration(
+                                    color: Colors.pink[100],
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  child: Column(
+                                    children: [
+                                      Text("Monthly Debit Orders"),
+
+                                      SizedBox(height: 20),
+
+                                      OutlinedButton(
+                                        onPressed: () {},
+                                        child: const Text("Cancel"),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
 
-                        Expanded(
-                          child: Center(
-                            child: Container(
-                              padding: const EdgeInsets.all(10.0),
-                              decoration: BoxDecoration(
-                                color: Colors.pink[100],
-                                borderRadius: BorderRadius.circular(20),
-                              ),
-                              child: Column(
-                                children: [
-                                  Text("Monthly Debit Orders"),
+                        SizedBox(height: 10),
 
-                                  SizedBox(height: 20),
-
-                                  OutlinedButton(
-                                    onPressed: () {},
-                                    child: const Text("Cancel"),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Expanded(
+                              child: Center(
+                                child: Container(
+                                  padding: const EdgeInsets.all(10.0),
+                                  decoration: BoxDecoration(
+                                    color: Colors.pink[100],
+                                    borderRadius: BorderRadius.circular(20),
                                   ),
-                                ],
+                                  child: Column(
+                                    children: [
+                                      Text("Monthly Debit Orders"),
+
+                                      SizedBox(height: 20),
+
+                                      OutlinedButton(
+                                        onPressed: () {},
+                                        child: const Text("Cancel"),
+                                      ),
+                                    ],
+                                  ),
+                                ),
                               ),
                             ),
-                          ),
+
+                            Expanded(
+                              child: Center(
+                                child: Container(
+                                  padding: const EdgeInsets.all(10.0),
+                                  decoration: BoxDecoration(
+                                    color: Colors.pink[100],
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  child: Column(
+                                    children: [
+                                      Text("Monthly Debit Orders"),
+
+                                      SizedBox(height: 20),
+
+                                      OutlinedButton(
+                                        onPressed: () {},
+                                        child: const Text("Cancel"),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ],
                     ),
-
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Expanded(
-                          child: Center(
-                            child: Container(
-                              padding: const EdgeInsets.all(10.0),
-                              decoration: BoxDecoration(
-                                color: Colors.pink[100],
-                                borderRadius: BorderRadius.circular(20),
-                              ),
-                              child: Column(
-                                children: [
-                                  Text("Monthly Debit Orders"),
-
-                                  SizedBox(height: 20),
-
-                                  OutlinedButton(
-                                    onPressed: () {},
-                                    child: const Text("Cancel"),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-
-                        Expanded(
-                          child: Center(
-                            child: Container(
-                              padding: const EdgeInsets.all(10.0),
-                              decoration: BoxDecoration(
-                                color: Colors.pink[100],
-                                borderRadius: BorderRadius.circular(20),
-                              ),
-                              child: Column(
-                                children: [
-                                  Text("Monthly Debit Orders"),
-
-                                  SizedBox(height: 20),
-
-                                  OutlinedButton(
-                                    onPressed: () {},
-                                    child: const Text("Cancel"),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
+                  ),
                 ),
-              ),
+              ],
             ),
           ),
         ),
---
+++ lib/uiTools.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class Uitools {
+  Uitools() {}
+
+  Container imgBtnTitleContainer(String title, String imgDir) {
+    return Container(
+      padding: const EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+        color: Colors.pink[100],
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        children: [Text(title), SizedBox(height: 10), btn1(imgDir)],
+      ),
+    );
+  }
+
+  OutlinedButton btn1(String imgDir) {
+    return OutlinedButton(
+      style: OutlinedButton.styleFrom(
+        backgroundColor: Colors.teal[50],
+        padding: const EdgeInsets.all(12),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+      ),
+
+      onPressed: () {},
+
+      child: Image.asset(imgDir, width: 80.0, height: 80.0),
+    );
+  }
+}
