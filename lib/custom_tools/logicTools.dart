76811d9dee0d59efb873eb2b9e155d4d6ce549f8
Bernard <judelsbernard2@gmail.com>
2026-08-08 15:31:16 +0200

    more logic to logicTools for removing items from other feature type lists

--- lib/custom_tools/logicTools.dart
+++ lib/custom_tools/logicTools.dart
@@ -1,5 +1,7 @@
 import 'package:first_flutter_app/models/DebitOrder.dart';
 import 'package:first_flutter_app/models/Service.dart';
+import 'package:first_flutter_app/models/DailyHabit.dart';
+import 'package:first_flutter_app/models/MedicalAid.dart';
 
 class LogicTools {
   const LogicTools();
@@ -15,5 +17,15 @@ class LogicTools {
   List<Service> serviceItemRemover(List<Service> services, int elementNum) {
     services.removeAt(elementNum);
     return services;
+  }
+
+  List<DailyHabit> dHabitItemRemover(List<DailyHabit> habits, int elementNum) {
+    habits.removeAt(elementNum);
+    return habits;
+  }
+
+  List<MedicalAid> medAidItemRemover(List<MedicalAid> meds, int elementNum) {
+    meds.removeAt(elementNum);
+    return meds;
   }
 }
