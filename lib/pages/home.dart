0b2f245656f791eadff3da5ee6220d1e6828c588
Bernard <judelsbernard2@gmail.com>
2026-08-14 22:06:17 +0200

    ui changes to home page and getting ready to publish test version 4 on google play internal testing

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -82,6 +82,7 @@ class _HomeState extends State<Home> {
   double habitTotal = 0;
 
   List<PieChartSectionData> pieChartSections(
+    double currentMonthTotal,
     double debitTotal,
     double serviceTotal,
     double medTotal,
@@ -96,27 +97,57 @@ class _HomeState extends State<Home> {
     return [
       PieChartSectionData(
         value: debitTotal,
-        color: Colors.red[400],
-        title: "Debit Orders",
-        radius: 80,
+        color: Colors.red[300],
+        borderSide: const BorderSide(
+          color: Colors.black,
+          width: 1,
+          style: BorderStyle.solid,
+        ),
+        title:
+            "${((debitTotal / currentMonthTotal) * 100).toStringAsFixed(0)}%",
+        titleStyle: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+
+        radius: 60,
       ),
       PieChartSectionData(
         value: serviceTotal,
-        color: Colors.lightBlue[400],
-        title: "Services",
-        radius: 80,
+        color: Colors.lightBlue[300],
+        borderSide: const BorderSide(
+          color: Colors.black,
+          width: 1,
+          style: BorderStyle.solid,
+        ),
+        title:
+            "${((serviceTotal / currentMonthTotal) * 100).toStringAsFixed(0)}%",
+        titleStyle: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+        radius: 60,
       ),
       PieChartSectionData(
         value: medTotal,
-        color: Colors.lightGreen[400],
-        title: "Medical Aid",
-        radius: 80,
+        color: Colors.lightGreen[300],
+        borderSide: const BorderSide(
+          color: Colors.black,
+          width: 1,
+          style: BorderStyle.solid,
+        ),
+        title: "${((medTotal / currentMonthTotal) * 100).toStringAsFixed(0)}%",
+        titleStyle: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+
+        radius: 60,
       ),
       PieChartSectionData(
         value: habitTotal,
-        color: Colors.purple[400],
-        title: "Daily Habits",
-        radius: 80,
+        color: Colors.purple[200],
+        borderSide: const BorderSide(
+          color: Colors.black,
+          width: 1,
+          style: BorderStyle.solid,
+        ),
+        title:
+            "${((habitTotal / currentMonthTotal) * 100).toStringAsFixed(0)}%",
+        titleStyle: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+
+        radius: 60,
       ),
     ];
   }
@@ -328,114 +359,281 @@ class _HomeState extends State<Home> {
                     borderRadius: BorderRadius.circular(20),
                     border: Border.all(color: Colors.black, width: 2),
                   ),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  child: Column(
                     children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Current Monthly Income:",
-                            style: const TextStyle(
-                              color: Colors.black,
-                              fontSize: 13,
-                            ),
+                      Center(
+                        child: Text(
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
                           ),
-                          Text(
-                            "R ${userSettings.isNotEmpty ? userSettings[0].getIncome.toStringAsFixed(2) : "0.00"}",
-                            style: const TextStyle(
-                              color: Colors.black,
-                              fontSize: 13,
-                            ),
-                          ),
-
-                          SizedBox(height: 3),
-
-                          Text(
-                            "Current Monthly Expenses:",
-                            style: const TextStyle(
-                              color: Colors.black,
-                              fontSize: 13,
-                            ),
-                          ),
-                          Text(
-                            "R ${currentMonthTotal.toStringAsFixed(2)}",
-                            style: const TextStyle(
-                              color: Colors.black,
-                              fontSize: 13,
-                            ),
-                          ),
-                          SizedBox(height: 3),
-
-                          Text(
-                            "End of Month Prediction:",
-                            style: const TextStyle(
-                              color: Colors.black,
-                              fontSize: 13,
-                            ),
-                          ),
-                          Text(
-                            "R ${endMonthPredict.toStringAsFixed(2)}",
-                            style: const TextStyle(
-                              color: Colors.black,
-                              fontSize: 13,
-                            ),
-                          ),
-                        ],
-                      ),
-
-                      const Spacer(),
-
-                      Column(
-                        children: [
-                          SizedBox(
-                            width: 130,
-                            child: uiTools.imgBtnTitleContainer2(
-                              "Select Month: ${_monthName(selectedMonth)} $selectedYear",
-                              "images/calendar1.png",
-                              selectMonthAndYear,
-                            ),
-                          ),
-
-                          const SizedBox(height: 5),
-
-                          Text(
-                            "${_monthName(selectedMonth)}: $daysInMonth days.",
-                            style: const TextStyle(
-                              fontSize: 12,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-
-                Container(
-                  width: double.infinity,
-                  margin: EdgeInsets.all(10.0),
-                  padding: const EdgeInsets.all(10.0),
-                  decoration: BoxDecoration(
-                    color: Colors.grey[100],
-                    borderRadius: BorderRadius.circular(20),
-                    border: Border.all(color: Colors.black, width: 2),
-                  ),
-
-                  child: Center(
-                    child: SizedBox(
-                      height: 300,
-                      child: PieChart(
-                        PieChartData(
-                          sections: pieChartSections(
-                            debitTotal,
-                            serviceTotal,
-                            medTotal,
-                            habitTotal,
-                          ),
+                          "Total Monthly Expenses:",
                         ),
                       ),
-                    ),
+
+                      SizedBox(height: 15),
+
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Current Monthly Income:",
+                                style: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 13,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                "R ${userSettings.isNotEmpty ? userSettings[0].getIncome.toStringAsFixed(2) : "0.00"}",
+                                style: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 13,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+
+                              SizedBox(height: 5),
+
+                              Text(
+                                "Current Monthly Expenses:",
+                                style: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 13,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                "R ${currentMonthTotal.toStringAsFixed(2)}",
+                                style: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 13,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              SizedBox(height: 5),
+
+                              Text(
+                                "End of Month Prediction:",
+                                style: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 13,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                "R ${endMonthPredict.toStringAsFixed(2)}",
+                                style: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 13,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+
+                          const Spacer(),
+
+                          Column(
+                            children: [
+                              SizedBox(
+                                width: 130,
+                                child: uiTools.imgBtnTitleContainer2(
+                                  "Select Month: ${_monthName(selectedMonth)} $selectedYear",
+                                  "images/calendar1.png",
+                                  selectMonthAndYear,
+                                ),
+                              ),
+
+                              const SizedBox(height: 5),
+
+                              Text(
+                                "${_monthName(selectedMonth)}: $daysInMonth days.",
+                                style: const TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+
+                      SizedBox(height: 10),
+                      Container(
+                        width: double.infinity,
+                        height: 2,
+                        decoration: BoxDecoration(
+                          color: Colors.grey[600],
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+
+                      SizedBox(height: 15),
+
+                      Center(
+                        child: Text(
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          "Monthly Expenses Per Category:",
+                        ),
+                      ),
+
+                      SizedBox(height: 15),
+
+                      SizedBox(
+                        height: 180,
+                        child: Row(
+                          children: [
+                            Expanded(
+                              flex: 4,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Expanded(
+                                    child: Container(
+                                      width: double.infinity,
+                                      margin: const EdgeInsets.only(bottom: 4),
+                                      decoration: BoxDecoration(
+                                        color: Colors.red[300],
+                                        borderRadius: BorderRadius.circular(5),
+                                        border: Border.all(
+                                          color: Colors.black,
+                                          width: 1,
+                                        ),
+                                      ),
+                                      child: Center(
+                                        child: Text(
+                                          style: TextStyle(
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                          "Debit Total:\nR${debitTotal.toStringAsFixed(2)}",
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+
+                                  Expanded(
+                                    child: Container(
+                                      width: double.infinity,
+                                      margin: const EdgeInsets.only(bottom: 4),
+                                      decoration: BoxDecoration(
+                                        color: Colors.lightBlue[300],
+                                        borderRadius: BorderRadius.circular(5),
+                                        border: Border.all(
+                                          color: Colors.black,
+                                          width: 1,
+                                        ),
+                                      ),
+                                      child: Center(
+                                        child: Text(
+                                          style: TextStyle(
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                          "Service Total:\nR${serviceTotal.toStringAsFixed(2)}",
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+
+                                  Expanded(
+                                    child: Container(
+                                      width: double.infinity,
+                                      margin: const EdgeInsets.only(bottom: 4),
+                                      decoration: BoxDecoration(
+                                        color: Colors.lightGreen[300],
+                                        borderRadius: BorderRadius.circular(5),
+                                        border: Border.all(
+                                          color: Colors.black,
+                                          width: 1,
+                                        ),
+                                      ),
+                                      child: Center(
+                                        child: Text(
+                                          style: TextStyle(
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                          "Insurance Total:\nR${medTotal.toStringAsFixed(2)}",
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+
+                                  Expanded(
+                                    child: Container(
+                                      width: double.infinity,
+                                      decoration: BoxDecoration(
+                                        color: Colors.purple[200],
+                                        borderRadius: BorderRadius.circular(5),
+                                        border: Border.all(
+                                          color: Colors.black,
+                                          width: 1,
+                                        ),
+                                      ),
+                                      child: Center(
+                                        child: Text(
+                                          style: TextStyle(
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                          "Daily Habits Total:\nR${habitTotal.toStringAsFixed(2)}",
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+
+                            const SizedBox(width: 10),
+
+                            Container(
+                              width: 2,
+                              height: 160,
+                              decoration: BoxDecoration(
+                                color: Colors.grey[600],
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+
+                            Expanded(
+                              flex: 5,
+                              child: Center(
+                                child: SizedBox(
+                                  width: 160,
+                                  height: 160,
+                                  child: PieChart(
+                                    PieChartData(
+                                      sectionsSpace: 1,
+                                      sections: pieChartSections(
+                                        currentMonthTotal,
+                                        debitTotal,
+                                        serviceTotal,
+                                        medTotal,
+                                        habitTotal,
+                                      ),
+                                      centerSpaceRadius: 20,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ),
 
